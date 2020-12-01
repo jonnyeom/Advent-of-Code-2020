21e121ed5d0dd5273c32a3d3732c194ce843b468
Jonathan Eom <jonnyeom@gmail.com>
2020-12-01 10:59:31 -0500

    Solve day 1 part 1

--- src/Day01.php
+++ src/Day01.php
@@ -6,8 +6,30 @@ namespace UnleashedTech\AdventOfCode2020;
 
 final class Day01
 {
-    public function solvePuzzle(string $input): string
+    public function solvePuzzlePart1(array $expenseReport)
     {
-        return 'Replace this with the code needed to get your answer';
+        foreach ($expenseReport as $key => $item) {
+            $topItem = array_shift($expenseReport);
+            $result[] = count($expenseReport);
+
+            $match = $this->findMatch($topItem, $expenseReport);
+            if ($match) {
+                return $item * $match;
+            }
+        }
+
+        return false;
+    }
+
+    private function findMatch (int $item, array $expenseReport)
+    {
+        foreach ($expenseReport as $candidate) {
+            if ($item + $candidate === 2020)
+            {
+                return $candidate;
+            }
+        }
+
+        return false;
     }
 }
